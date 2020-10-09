a5128ab64f00b9156b4d0d868c1ec6785babd0c1
Sven Gothel <sgothel@jausoft.com>
2020-10-09 02:15:53 +0200

    run-dbt-scanner10.sh: Added current valgrind (memcheck, drd and callgrind) commented-out options
    Example with callgrind enabled: "../scripts/run-dbt_scanner10.sh -log ~/dbt_scanner10-arm64-30 -dev_id 1 -disconnect -count 1 -Ndebug -dbt_debug false -quiet -mac C0:26:DA:01:DA:B1 -mac C0:26:DF:01:E5:CA -mac C0:26:DA:00:03:DC -mac C0:26:DA:05:EF:5B"

--- scripts/run-dbt_scanner10.sh
+++ scripts/run-dbt_scanner10.sh
@@ -15,15 +15,28 @@
 sdir=`dirname $(readlink -f $0)`
 rootdir=`dirname $sdir`
 bname=`basename $0 .sh`
-logfile=$bname.log
-rm -f $logfile
-valgrindlogfile=$bname-valgrind.log
-rm -f $valgrindlogfile
 
 if [ ! -e bin/dbt_scanner00 -o ! -e lib/libdirect_bt.so ] ; then
     echo run from dist directory
     exit 1
 fi
+
+if [ "$1" = "-log" ] ; then
+    logbasename=$2
+    shift 2
+else
+    logbasename=~/$bname
+fi
+
+logfile=$logbasename.log
+rm -f $logfile
+
+valgrindlogfile=$logbasename-valgrind.log
+rm -f $valgrindlogfile
+
+callgrindoutfile=$logbasename-callgrind.out
+rm -f $callgrindoutfile
+
 # hciconfig hci0 reset
 ulimit -c unlimited
 
@@ -31,12 +44,25 @@ ulimit -c unlimited
 export LANG=en_US.UTF-8
 export LC_MEASUREMENT=en_US.UTF-8
 
-echo COMMANDLINE $0 $*
-echo direct_bt_debug $direct_bt_debug
-echo direct_bt_verbose $direct_bt_verbose
-
-# VALGRIND="valgrind --leak-check=full --show-reachable=yes --error-limit=no --default-suppressions=yes --suppressions=$sdir/valgrind.supp --gen-suppressions=all -s --log-file=$valgrindlogfile"
+# VALGRIND="valgrind --tool=memcheck --leak-check=full --show-reachable=yes --error-limit=no --default-suppressions=yes --suppressions=$sdir/valgrind.supp --gen-suppressions=all -s --log-file=$valgrindlogfile"
 # VALGRIND="valgrind --tool=helgrind --track-lockorders=yes  --ignore-thread-creation=yes --default-suppressions=yes --suppressions=$sdir/valgrind.supp --gen-suppressions=all -s --log-file=$valgrindlogfile"
+# VALGRIND="valgrind --tool=drd --segment-merging=no --ignore-thread-creation=yes --trace-barrier=no --trace-cond=no --trace-fork-join=no --trace-mutex=no --trace-rwlock=no --trace-semaphore=no --default-suppressions=yes --suppressions=$sdir/valgrind.supp --gen-suppressions=all -s --log-file=$valgrindlogfile"
+# VALGRIND="valgrind --tool=callgrind --instr-atstart=yes --collect-atstart=yes --collect-systime=yes --combine-dumps=yes --separate-threads=no --callgrind-out-file=$callgrindoutfile --log-file=$valgrindlogfile"
 
-#LD_LIBRARY_PATH=`pwd`/lib strace bin/dbt_scanner10 $*
-LD_LIBRARY_PATH=`pwd`/lib $VALGRIND bin/dbt_scanner10 $*
+runit() {
+    echo COMMANDLINE $0 $*
+    echo VALGRIND $VALGRIND
+    echo logbasename $logbasename
+    echo logfile $logfile
+    echo valgrindlogfile $valgrindlogfile
+    echo callgrindoutfile $callgrindoutfile
+    echo direct_bt_debug $direct_bt_debug
+    echo direct_bt_verbose $direct_bt_verbose
+
+    #LD_LIBRARY_PATH=`pwd`/lib strace bin/dbt_scanner10 $*
+    echo LD_LIBRARY_PATH=`pwd`/lib $VALGRIND bin/dbt_scanner10 $*
+    LD_LIBRARY_PATH=`pwd`/lib $VALGRIND bin/dbt_scanner10 $*
+}
+
+runit $* 2>&1 | tee $logfile
+
