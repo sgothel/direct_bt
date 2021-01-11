d874fad4b312d5d404386e61be5fc3c0253b9560
Sven Gothel <sgothel@jausoft.com>
2021-01-11 15:05:39 +0100

    Fix unit tests' config for main

--- test/direct_bt/test_uuid.cpp
+++ test/direct_bt/test_uuid.cpp
@@ -3,7 +3,8 @@
 #include <cinttypes>
 #include <cstring>
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+// #define CATCH_CONFIG_MAIN
 #include <catch2/catch_amalgamated.hpp>
 #include <jau/test/catch2_ext.hpp>
 
