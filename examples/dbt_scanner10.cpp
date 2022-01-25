0c14d64020bab9ec7925f351e63365ab5784d6c2
Sven Gothel <sgothel@jausoft.com>
2022-01-25 15:31:54 +0100

    dbt_scanner10: Adapt to EInfoReport change (shared_ptr)

--- examples/dbt_scanner10.cpp
+++ examples/dbt_scanner10.cpp
@@ -395,14 +395,14 @@ static void connectDiscoveredDevice(BTDeviceRef device) {
             fprintf_td(stderr, "****** Connecting Device: Setting SEC AUTO security detail w/ KEYBOARD_ONLY -> set OK %d\n", r);
         }
     }
-    const EInfoReport& eir = device->getEIR();
-    fprintf_td(stderr, "Using EIR %s\n", eir.toString().c_str());
+    std::shared_ptr<const EInfoReport> eir = device->getEIR();
+    fprintf_td(stderr, "Using EIR %s\n", eir->toString().c_str());
 
     uint16_t conn_interval_min  = (uint16_t)12;
     uint16_t conn_interval_max  = (uint16_t)12;
     const uint16_t conn_latency  = (uint16_t)0;
-    if( eir.isSet(EIRDataType::CONN_IVAL) ) {
-        eir.getConnInterval(conn_interval_min, conn_interval_max);
+    if( eir->isSet(EIRDataType::CONN_IVAL) ) {
+        eir->getConnInterval(conn_interval_min, conn_interval_max);
     }
     const uint16_t supervision_timeout = (uint16_t) getHCIConnSupervisorTimeout(conn_latency, (int) ( conn_interval_max * 1.25 ) /* ms */);
     res = device->connectLE(le_scan_interval, le_scan_window, conn_interval_min, conn_interval_max, conn_latency, supervision_timeout);
