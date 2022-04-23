77e86ffecd195c546c2b9138d67ace39d4db1d97
Sven Gothel <sgothel@jausoft.com>
2022-04-24 01:32:16 +0200

    scripts/run-trials-until.sh: Add notice to run from build directory (if not)

--- scripts/run-trials-until.sh
+++ scripts/run-trials-until.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -e trial/java/direct_bt-trial.jar ] ; then
+    echo run from build directory
+    exit 1
+fi
+
 run_until() {
     let n=0; while make test ; do let n=${n}+1; echo "Test ${n} OK"; cp -av Testing/Temporary/LastTest.log LastTest-${n}.log ; done
 }
