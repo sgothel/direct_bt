c2d430cd94c7a19a99de64249070420db6277818
Sven Göthel <sgothel@jausoft.com>
2024-05-12 09:15:45 +0200

    scripts/rebuild-doc.sh: Make it work with new cmake build setup

--- scripts/rebuild-doc.sh
+++ scripts/rebuild-doc.sh
@@ -4,43 +4,37 @@ sdir=`dirname $(readlink -f $0)`
 rootdir=`dirname $sdir`
 bname=`basename $0 .sh`
 
-. $rootdir/jaulib/scripts/setup-machine-arch.sh
+build_dir=$1
 
-logfile=$rootdir/$bname-$os_name-$archabi.log
+if [ -z "$build_dir" ] ; then
+    echo "Pass build directory as first argument"
+    exit 1
+fi
+
+logfile=$rootdir/$bname.log
 rm -f $logfile
-
-CPU_COUNT=`getconf _NPROCESSORS_ONLN`
 
 # run 'dpkg-reconfigure locales' enable 'en_US.UTF-8'
 export LANG=en_US.UTF-8
 export LC_MEASUREMENT=en_US.UTF-8
 
 buildit() {
-    if [ -z "$JAVA_HOME" -o ! -e "$JAVA_HOME" ] ; then
-        echo "WARNING: JAVA_HOME $JAVA_HOME does not exist"
-    else
-        echo JAVA_HOME $JAVA_HOME
-    fi
     echo rootdir $rootdir
     echo logfile $logfile
-    echo CPU_COUNT $CPU_COUNT
 
-    dist_dir="dist-$os_name-$archabi"
-    build_dir="build-$os_name-$archabi"
-    echo dist_dir $dist_dir
     echo build_dir $build_dir
 
     cd $rootdir/$build_dir
     rm -rf documentation
-    make -j $CPU_COUNT doc
+    cmake --build . --target doc_jau --parallel
     if [ $? -eq 0 ] ; then
-        echo "REBUILD SUCCESS $bname $os_name $archabi"
+        echo "REBUILD SUCCESS $bname"
         rm -f $rootdir/documentation.tar.xz
         tar caf $rootdir/documentation.tar.xz documentation
         cd $rootdir
         return 0
     else
-        echo "REBUILD FAILURE $bname $os_name $archabi"
+        echo "REBUILD FAILURE $bname"
         cd $rootdir
         return 1
     fi
